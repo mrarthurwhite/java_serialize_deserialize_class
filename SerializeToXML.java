266f9e7e3d833a5a0b749b70baaf488ea97a2bc8
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-08 08:27:31 -0400

    adding class structure

--- SerializeToXML.java
+++ SerializeToXML.java
@@ -1,1 +1,24 @@
+/**
+ * 
+ */
+import java.io.File;
 
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+/**
+ * @author SOLON
+ *
+ */
+public class SerializeToXML {
+
+	static String outputContactsFile = "contacts.xml";//output contacts to xml
+
+	/**
+	 * @param args
+	 */
+
+
+
+}
